0f64447de1ff28acff07073dbb15f04cb5038bd3
Алексей Шашин <alexshashin.2023@gmail.com>
2025-11-05 19:08:49 +0500

    Реализованные интерфейсы. Шашин.

---
+++ Interfaces/IDocument.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace documents_shashin.Interfaces
+{
+    public interface IDocument
+    {
+        void Save(bool Update = false);
+        List<documents_shashin.Classes.DocumentContext> AllDocuments();
+        void Delete();
+    }
+}
